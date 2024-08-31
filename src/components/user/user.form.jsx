e279e739a82e89140191181f7ea008c5a826e175
DangHoangHai <danghoanghai2907@gmail.com>
2024-08-31 17:31:37 +0700

    Get Data Form

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,8 +1,16 @@
  
  import { Input } from 'antd';
  import { Button } from 'antd';
-import Password from 'antd/es/input/Password';
+import { useState } from 'react';
  const UserForm = () =>{
+    const [FullName , SetFullName] = useState("")
+    const [Email , SetEmail] = useState("")
+    const [Password , SetPassword] = useState("")
+    const [PhoneNumber , SetPhoneNumber] = useState("")
+   
+    const handleClickBtn = () =>{
+    console.log("ok" ,{ FullName , Email , Password , PhoneNumber})
+    }
 return(
     <div className="user-form" style={{width : "50%" , margin : "auto"}}>
             <div style={{
@@ -11,24 +19,37 @@ return(
                 gap : "15px"
             }}>
                 <div>
-                    <span>FullName</span>
-                    <Input></Input>
+                    <span>FullName</span>           
+                    <Input
+                    value={FullName}
+                    onChange={(event) => SetFullName(event.target.value)}
+                    />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input></Input>
+                    <Input
+                    value={Email}
+                    onChange={(event) => SetEmail(event.target.value)}
+                    />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Password></Password>
+                    <Input.Password
+                    value={Password}
+                    onChange={(event) => SetPassword(event.target.value)}
+                    />
                 </div>
                 <div>
                     <span>PhoneNumber</span>
-                    <Input></Input>
+                    <Input
+                    value={PhoneNumber}
+                    onChange={(event) => SetPhoneNumber(event.target.value)}
+                    />
                 </div>
                 
                 <div>
-                    <Button type="primary" >Create User</Button>
+                    <Button type="primary" 
+                    onClick={handleClickBtn}>Create User</Button>
                  </div>
             </div>
             
